c407012d2068920111d1933d9e02ebd80f8592ce
waxo123 <67927059+waxo123@users.noreply.github.com>
2020-08-26 11:16:25 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  TOKEN: "NzMyNjAxMTI2MTcxNDQzMjQw.Xw299w.QLYb7uE7PPq20aYabyq6g5PS88A", //تعديل مهم توكن بوتك
-  YT_API_KEY: "api ",
-  prefix: "+",
-  devs: ["565833332638875648"] ///تعديل مهم ايدي حسابك
-};
